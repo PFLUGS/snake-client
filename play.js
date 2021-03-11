f03f958e2b14c9c6d38e66a3d3d819865125f6a8
Sarah <sarah.pflug@shopify.com>
2021-03-11 02:52:04 +0000

    added input file and modified play.js

--- play.js
+++ play.js
@@ -3,23 +3,10 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-
-}
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const connect = require('./client');
-console.log('Connecting ...');
-connect();
-setupInput();
+ const { connect } = require('./client');
+ const { setupInput } = require('./input');
+ 
+ console.log('Connecting ...');
+ connect();
+ 
+ setupInput();
